b059d04a3a58eb8fc9c48fbf865dd9e546ee15c5
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-12-18 15:38:13 +0530

    Add add plugin and dependencies source

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "ExploringTheWorldOfKotlin"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
