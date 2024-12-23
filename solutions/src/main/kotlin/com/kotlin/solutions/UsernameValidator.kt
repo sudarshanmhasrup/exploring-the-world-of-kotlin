3bac258a96007b1a4a9f72496ebebff401efcd85
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-12-23 15:09:02 +0530

    Create username validator class

---
+++ solutions/src/main/kotlin/com/kotlin/solutions/UsernameValidator.kt
@@ -0,0 +1,13 @@
+package com.kotlin.solutions
+
+class UsernameValidator {
+
+    // Function to validate username
+    fun validateUsername(username: String): Boolean {
+        if (username.length < 3) {
+            return false
+        }
+
+        return false
+    }
+}
