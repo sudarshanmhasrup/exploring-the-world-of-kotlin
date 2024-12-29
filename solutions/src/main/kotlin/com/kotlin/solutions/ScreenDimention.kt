e3f1685e6cbd9e198d138cdaae630ebb673bdd87
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-12-29 15:57:35 +0530

    Create a program to get window dimensions

---
+++ solutions/src/main/kotlin/com/kotlin/solutions/ScreenDimention.kt
@@ -0,0 +1,13 @@
+package com.kotlin.solutions
+
+import java.awt.Toolkit
+
+
+fun main() {
+
+    val windowToolkit = Toolkit.getDefaultToolkit().screenSize
+    val windowWidth = windowToolkit.width
+    val windowHeight = windowToolkit.height
+
+    println("Width: $windowWidth\n Height: $windowHeight")
+}
