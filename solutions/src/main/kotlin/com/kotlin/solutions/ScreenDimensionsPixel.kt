f1a3267fa947b7a33029ffd172993dd6d93d2562
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-12-29 16:02:38 +0530

    Create logic to get screen dimensions in pixels

--- solutions/src/main/kotlin/com/kotlin/solutions/ScreenDimensionsPixel.kt
+++ solutions/src/main/kotlin/com/kotlin/solutions/ScreenDimensionsPixel.kt
@@ -2,12 +2,12 @@ package com.kotlin.solutions
 
 import java.awt.Toolkit
 
-
 fun main() {
 
     val windowToolkit = Toolkit.getDefaultToolkit().screenSize
     val windowWidth = windowToolkit.width
     val windowHeight = windowToolkit.height
 
-    println("Width: $windowWidth\n Height: $windowHeight")
+    println("Width: $windowWidth")
+    println("Height: $windowHeight")
 }
