66665c394a69d7ef2129c0ee962cca06e42d32c2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-02-18 10:08:12 +0530

    Set JVM target to 11

--- practiceSolutions/build.gradle.kts
+++ practiceSolutions/build.gradle.kts
@@ -1,5 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/practice solutions"))
