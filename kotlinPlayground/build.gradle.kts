e6395eb36579c1dd6243ef3bde129b126c4b9626
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-02-18 11:59:40 +0530

    Define JVM targets

--- kotlinPlayground/build.gradle.kts
+++ kotlinPlayground/build.gradle.kts
@@ -1,3 +1,16 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
