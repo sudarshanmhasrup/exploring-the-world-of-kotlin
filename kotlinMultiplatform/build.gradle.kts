f84ccbf6e2812b0dd43f0b95c30358ce3ca36c8b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-03-14 15:38:14 +0530

    Define jvm target for kotlin multiplatform

--- kotlinMultiplatform/build.gradle.kts
+++ kotlinMultiplatform/build.gradle.kts
@@ -1,3 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
 }
