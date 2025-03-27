08303b7d7ed218bd2c2accc896c6c2fae5e5259d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-03-27 22:18:14 +0530

    Declare plugins to top level build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.compiler) apply false
 }
