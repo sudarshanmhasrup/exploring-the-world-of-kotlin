5a9c1d50c76b1f558bc295878727b14adb5e7725
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-03-29 12:30:07 +0530

    Configure test task for practice solutions module

--- practiceSolutions/build.gradle.kts
+++ practiceSolutions/build.gradle.kts
@@ -6,5 +6,9 @@ dependencies {
     testImplementation(libs.kotlin.test)
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/practiceSolutions"))
