5382d77a89bbce6166504842953bfd0f60e9278c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-03-29 12:29:24 +0530

    Add "org.jetbrains.kotlin:kotlin-test" dependency to practice solutions module

--- practiceSolutions/build.gradle.kts
+++ practiceSolutions/build.gradle.kts
@@ -2,5 +2,9 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
+dependencies {
+    testImplementation(libs.kotlin.test)
+}
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/practiceSolutions"))
