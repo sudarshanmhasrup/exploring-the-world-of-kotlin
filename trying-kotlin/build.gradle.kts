b856f7e4a9994dcfdda497b3c76a3a53bdf71d84
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-02 11:43:20 +0530

    Setup trying-kotlin module

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -1,5 +1,20 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.compiler)
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 // Custom build directory
