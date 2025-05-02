5c656a1a878c0352b104a58a29151211aca423ef
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-02 10:19:24 +0530

    Create Program 2

---
+++ practice-solutions/src/main/kotlin/com/practice/solutions/Program2.kt
@@ -0,0 +1,34 @@
+package com.practice.solutions
+
+import com.practice.solutions.util.UserInputManager
+import com.practice.solutions.util.UserInputType
+
+/*
+    Problem statement: Write a function that takes an integer minutes and converts it to seconds.
+    Modified: True.
+    Source: https://edabit.com/challenge/8q54MKnRrm89pSLmW.
+ */
+
+fun main() {
+    val minutes = getUserInput()
+    displayOutput(minutes = minutes)
+}
+
+private fun getUserInput(): Int {
+    return UserInputManager.getUserInput(
+        placeholder = "Enter minutes in digit",
+        error = "Invalid input! Please enter an integer value.",
+        userInputType = UserInputType.INTEGER
+    ) as Int
+}
+
+private fun displayOutput(minutes: Int) {
+    val result = minutesToSeconds(minutes = minutes)
+    val minutesSuffix = if (minutes == 1) "minute" else "minutes"
+
+    println("$minutes $minutesSuffix is same as $result seconds")
+}
+
+internal fun minutesToSeconds(minutes: Int): Int {
+    return minutes * 60
+}
