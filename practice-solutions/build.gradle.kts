f3d7a78a1da0dcfc98d982a602c261855b01f62b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-02 07:47:25 +0530

    Setup practice-solutions module

--- practice-solutions/build.gradle.kts
+++ practice-solutions/build.gradle.kts
@@ -1,5 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
+}
+
+dependencies {
+    
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 // Custom build directory
