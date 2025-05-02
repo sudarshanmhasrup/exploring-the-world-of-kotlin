4e405d8f689aa36ff2906a47de745c3d6b2a255f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-02 10:27:01 +0530

    Create Program 3 and setup tests

---
+++ practice-solutions/src/main/kotlin/com/practice/solutions/Program3.kt
@@ -0,0 +1,31 @@
+package com.practice.solutions
+
+import com.practice.solutions.util.UserInputManager
+import com.practice.solutions.util.UserInputType
+
+/*
+    Problem statement: Create a function that takes a number as an argument, increments the number by +1 and returns the
+    result.
+    Modified: True.
+    Source: https://edabit.com/challenge/NAQhEoxbofPidLxm9.
+ */
+
+fun main() {
+    val number = getUserInput()
+    display(number = number)
+}
+
+private fun getUserInput(): Int {
+    return UserInputManager.getUserInput(
+        placeholder = "Enter a number",
+        error = "Invalid input! Please enter an integer value.",
+        userInputType = UserInputType.INTEGER
+    ) as Int
+}
+
+private fun display(number: Int) {
+    val result = incrementNumberByOne(number = number)
+    println("The number that comes after $number is $result.")
+}
+
+internal fun incrementNumberByOne(number: Int): Int = number + 1
