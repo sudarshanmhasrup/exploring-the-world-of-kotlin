9fe95026113819e068a9c22e14d1875539c19094
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-03 13:45:17 +0530

    Modify Program3.kt

--- practice-solutions/src/main/kotlin/com/practice/solutions/Program3.kt
+++ practice-solutions/src/main/kotlin/com/practice/solutions/Program3.kt
@@ -4,7 +4,7 @@ import com.practice.solutions.util.UserInputManager
 import com.practice.solutions.util.UserInputType
 
 /*
-    Problem statement: Create a function that takes a number as an argument, increments the number by +1 and returns the
+    Problem statement: Create a function that takes a number as an argument, increments the number by 1 and returns the
     result.
     Modified: True.
     Source: https://edabit.com/challenge/NAQhEoxbofPidLxm9.
