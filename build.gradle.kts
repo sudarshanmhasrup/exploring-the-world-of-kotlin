183e3ed063b95ed89d93ec1a60cad84a2d9c9ec7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-05 19:14:37 +0530

    Add "com.android.application" plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application) apply false
 }
 
 // Custom build directory
