b4f44acfa7ce4b060c3c4c7cd68766f73a86a5b8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-05 19:24:08 +0530

    Add Android app package information to android-app module

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,3 +2,17 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.application)
 }
+
+android {
+    compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+    namespace = libs.versions.androidApp.applicationId.get()
+
+    defaultConfig {
+        applicationId = libs.versions.androidApp.applicationId.get()
+        compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+        targetSdk = libs.versions.androidApp.targetSdkVersion.get().toInt()
+        minSdk = libs.versions.androidApp.minSdkVersion.get().toInt()
+        version = libs.versions.androidApp.versionCode.get().toInt()
+        versionName = libs.versions.androidApp.versionName.get()
+    }
+}
