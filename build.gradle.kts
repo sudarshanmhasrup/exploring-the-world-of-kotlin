86ba6cee654d87e2fef4850a2ff16ca372a7303f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-06 15:05:41 +0530

    Add "org.jetbrains.kotlin.multiplatform" plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
