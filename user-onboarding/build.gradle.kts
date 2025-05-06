63ec6ef47e3992e1ca94eb7fb87dfa09fc41a7fb
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-06 15:38:26 +0530

    Add "org.jetbrains.compose.ui:ui" dependency to version catalog

--- user-onboarding/build.gradle.kts
+++ user-onboarding/build.gradle.kts
@@ -17,6 +17,12 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.ui)
+        }
+    }
 }
 
 android {
