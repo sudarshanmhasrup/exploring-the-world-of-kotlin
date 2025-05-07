6916c6336c9e24d15b2d15c055c56617a3e3bdf1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-07 21:44:22 +0530

    Setup compose-desktop app module to use compose for desktop

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -1,3 +1,30 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.compiler)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(project(":compose-app"))
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.compose.desktop.MainKt"
+    }
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/desktopApp"))
