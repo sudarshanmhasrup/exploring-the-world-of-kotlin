9deb1dcb6e19d4f7911882c8de73f0f17d871267
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-07 20:29:22 +0530

    Refactor: Introduce Platform to ComposeAppTheme

--- design-system/src/commonMain/kotlin/com/design/system/api/ComposeAppTheme.kt
+++ design-system/src/commonMain/kotlin/com/design/system/api/ComposeAppTheme.kt
@@ -3,6 +3,7 @@ package com.design.system.api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import com.design.system.colorScheme.Colors
+import com.design.system.platform.Platform
 import com.design.system.typography.Typography
 
 object ComposeAppTheme {
@@ -12,17 +13,22 @@ object ComposeAppTheme {
     val typography: Typography
         @Composable
         get() = LocalTypography.current
+    val platform: Platform
+        @Composable
+        get() = LocalPlatform.current
 }
 
 @Composable
 fun ComposeAppTheme(
     colors: Colors,
     typography: Typography,
+    platform: Platform,
     content: @Composable () -> Unit
 ) {
     CompositionLocalProvider(
         LocalColors provides colors,
-        LocalTypography provides typography
+        LocalTypography provides typography,
+        LocalPlatform provides platform
     ) {
         content()
     }
