b6667933f2ceed44a79ac6d4a96e725cc3a2a82d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-07 18:12:08 +0530

    Update backgroundContainerModifier to set background color and fill max size of container.

--- compose-app/src/commonMain/kotlin/com/compose/app/extensions/Modifiers.kt
+++ compose-app/src/commonMain/kotlin/com/compose/app/extensions/Modifiers.kt
@@ -1,18 +1,12 @@
 package com.compose.app.extensions
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
-internal fun Modifier.backgroundContainerModifier(
-    color: Color,
-    scrollState: ScrollState
-): Modifier {
+internal fun Modifier.backgroundContainerModifier(color: Color): Modifier {
     return this
         .background(color = color)
         .fillMaxSize()
-        .verticalScroll(state = scrollState)
 }
