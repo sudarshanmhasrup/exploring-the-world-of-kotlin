bceb0a57a4eeef399678017db1fc72a78d178026
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-08 21:19:06 +0530

    Setup shared-components module

--- shared-components/build.gradle.kts
+++ shared-components/build.gradle.kts
@@ -1,3 +1,37 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
+
+group = libs.versions.userOnboarding.groupId.get()
+version = libs.versions.userOnboarding.version.get()
+
+kotlin {
+    // Declaring dummy targets to avoid build warnings
+    jvm("desktop")
+
+    androidTarget()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplatform.resources)
+            implementation(libs.compose.multiplatform.foundation)
+            implementation(libs.compose.multiplatform.ui)
+            implementation(project(":design-system"))
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.userOnboarding.groupId.get()
+    compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/sharedComponents"))
