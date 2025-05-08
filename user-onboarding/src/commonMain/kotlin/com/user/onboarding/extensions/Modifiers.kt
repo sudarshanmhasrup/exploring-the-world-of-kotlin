989acfb50f41048cd1f6382eb4d31d3470c5704a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-08 15:41:48 +0530

    Refactor modifiers for landing page UI elements.

--- user-onboarding/src/commonMain/kotlin/com/user/onboarding/extensions/Modifiers.kt
+++ user-onboarding/src/commonMain/kotlin/com/user/onboarding/extensions/Modifiers.kt
@@ -8,13 +8,46 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun Modifier.landingPageImageModifier(): Modifier {
+    return this.fillMaxWidth().padding(all = 20.dp)
+}
+
+@Composable
+internal fun Modifier.landingPageGreetingAndMessageModifier(width: Double): Modifier {
+    return this
+        .fillMaxWidth()
+        .landingPageGreetingPadding(width = width)
+}
+
+@Composable
+internal fun Modifier.landingPageGreetingPadding(width: Double): Modifier {
+    val widthInNumber = width.toInt()
+    return when {
+        // Break-point for tablets
+        widthInNumber in 760..1199 -> {
+            val paddingStartAndEnd = (width * 0.15).dp
+            padding(start = paddingStartAndEnd, end = paddingStartAndEnd, top = 120.dp)
+        }
+        // Break-point for desktop
+        widthInNumber >= 1200 -> {
+            val paddingStartAndEnd = (width * 0.25).dp
+            padding(start = paddingStartAndEnd, end = paddingStartAndEnd, top = 140.dp)
+        }
+        // Break-point for mobile
+        else -> {
+            padding(all = 20.dp)
+        }
+    }
+}
+
+@Composable
+internal fun Modifier.landingPageButtonsColumnModifier(): Modifier {
     return this
         .fillMaxWidth()
         .padding(all = 20.dp)
 }
 
 @Composable
-internal fun Modifier.landingPageGreetingAndMessageModifier(): Modifier {
+internal fun Modifier.landingPageButtonsRowModifier(): Modifier {
     return this
         .fillMaxWidth()
         .padding(all = 20.dp)
