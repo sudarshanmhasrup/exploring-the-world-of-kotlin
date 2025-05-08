afbb2bcd488361450eb45d2d5d7b6d3795a53f81
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-08 15:43:43 +0530

    Create LandingPageScreen model

---
+++ user-onboarding/src/commonMain/kotlin/com/user/onboarding/presentation/pages/LandingPageScreenModel.kt
@@ -0,0 +1,15 @@
+package com.user.onboarding.presentation.pages
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+internal class LandingPageScreenModel : ScreenModel {
+
+    private var _landingPageUiState = MutableStateFlow(LandingPageUiState())
+    val landingPageUiState = _landingPageUiState.asStateFlow()
+
+    fun updateLandingPageScreenWidth(width: Double) {
+        _landingPageUiState.value = _landingPageUiState.value.copy(landingPageWidth = width)
+    }
+}
