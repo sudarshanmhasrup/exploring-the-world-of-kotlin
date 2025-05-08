1c235e9be0f23a8e4bb59b1a823342677809c116
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-08 10:42:30 +0530

    Setup platform-apis module

--- platform-apis/build.gradle.kts
+++ platform-apis/build.gradle.kts
@@ -1,3 +1,26 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
 }
+
+group = libs.versions.platformApis.groupId.get()
+version = libs.versions.platformApis.version.get()
+
+kotlin {
+    // Declaring dummy targets to avoid build warnings
+    jvm("desktop")
+
+    androidTarget()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+}
+
+android {
+    namespace = libs.versions.platformApis.groupId.get()
+    compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/platformApis"))
