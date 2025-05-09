612645bbc8015e008ea04b760deea140f9fc541f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-09 19:15:43 +0530

    Setup Kotlin Playground to use Kotlin Environment

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,3 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    
+    alias(libs.plugins.kotlin.compiler)
 }
+
+group = libs.versions.tryingKotlin.groupId.get()
+version = libs.versions.tryingKotlin.version.get()
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/kotlinPlayground"))
