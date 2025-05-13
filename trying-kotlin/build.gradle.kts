097dc3b10c2a3188e57c9e3c02690b51260b5b05
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 10:28:32 +0530

    Set JVM version compatibility to JVM 11

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -1,6 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
 group = libs.versions.tryingKotlin.groupId.get()
 version = libs.versions.releaseVersion.get()
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
