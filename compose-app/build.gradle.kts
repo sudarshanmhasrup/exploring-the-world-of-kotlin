fcddd2a3778a8abb5366fb3ea9d602256aed1afd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 20:10:28 +0530

    Add "cafe.adriel.voyager:voyager-screenmodel" dependency to compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.voyager.navigator)
+            implementation(libs.voyager.screenmodel)
             implementation(libs.compose.multiplatform.ui)
         }
     }
