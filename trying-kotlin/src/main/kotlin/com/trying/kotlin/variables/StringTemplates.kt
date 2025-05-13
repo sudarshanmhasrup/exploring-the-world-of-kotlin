b37b3c94c7666bc17039a2f5c24ea5ae4e0408c7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 11:58:27 +0530

    Create programs to understand use of string templates in Kotlin

---
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/variables/StringTemplates.kt
@@ -0,0 +1,6 @@
+package com.trying.kotlin.variables
+
+fun main() {
+    val name = "Sudarshan"
+    println("Hello from $name.") // Use of string template
+}
---
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/variables/TemplateExpression.kt
@@ -0,0 +1,6 @@
+package com.trying.kotlin.variables
+
+fun main() {
+    val name = "Sudarshan"
+    println("Hello my name is: ${name.uppercase()}!") // Use of template expression
+}
