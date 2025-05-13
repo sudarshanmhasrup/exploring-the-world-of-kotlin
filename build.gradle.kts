9fca53d13f15b5e5ed5278f0c4b828ad61a7ea03
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 13:00:51 +0530

    Add "org.jetbrains.kotlin.plugin.compose" plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.compiler) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/rootProject"))
