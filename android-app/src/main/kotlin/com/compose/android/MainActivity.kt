aea5fe1c35c452d019a204063954c737e3336872
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 19:36:52 +0530

    Create and add MainActivity

---
+++ android-app/src/main/kotlin/com/compose/android/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.compose.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
