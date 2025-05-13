c63bafe0152ac29402c31f2cc7c48d59f25c0a09
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 12:58:10 +0530

    Setup android environment for android-app module

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,3 +2,33 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.application)
 }
+
+group = libs.versions.androidApp.groupId.get()
+version = libs.versions.releaseVersion.get()
+
+android {
+    namespace = libs.versions.androidApp.applicationId.get()
+    compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.androidApp.applicationId.get()
+        compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+        targetSdk = libs.versions.androidApp.targetSdkVersion.get().toInt()
+        minSdk = libs.versions.androidApp.minSdkVersion.get().toInt()
+        version = libs.versions.androidApp.versionCode.get().toInt()
+        versionName = libs.versions.androidApp.versionName.get()
+    }
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/androidApp"))
