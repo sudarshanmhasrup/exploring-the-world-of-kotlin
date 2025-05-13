9296a2a61c9f392b896653ad43ef3ca5d9467878
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 20:02:14 +0530

    Setup compose multiplatform environment in compose app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,3 +1,34 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
+
+group = libs.versions.composeApp.groupId.get()
+version = libs.versions.releaseVersion.get()
+
+kotlin {
+    jvm("desktop")
+
+    androidTarget()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { platform ->
+        platform.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.composeApp.groupId.get()
+    compileSdk = libs.versions.androidApp.compileSdkVersion.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/composeApp"))
