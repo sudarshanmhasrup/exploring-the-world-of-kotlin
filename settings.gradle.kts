35f5f8431d295fec924016366ecb605463eb3148
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 10:04:18 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "ExploringTheWorldOfKotlin"
+
+@Suppress("UnstableApiUsage")
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
