4a1e1f7c81420a9b00b37eab3219abdfba305f9a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-13 12:08:10 +0530

    Setup practice-solutions module for Kotlin environment

--- practice-solutions/build.gradle.kts
+++ practice-solutions/build.gradle.kts
@@ -1,3 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
+    alias(libs.plugins.kotlin.compiler)
+}
+
+group = libs.versions.practiceSolutions.groupId.get()
+version = libs.versions.releaseVersion.get()
+
+dependencies {
 
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+layout.buildDirectory.set(file("$rootDir/.build/practiceSolutions"))
