e31a33bb59e61485e9ffc6f0be3ebe70ece7f0a3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-14 12:56:31 +0530

    Call AndroidApp.kt from MainActivity.kt

--- android-app/src/main/kotlin/com/compose/android/MainActivity.kt
+++ android-app/src/main/kotlin/com/compose/android/MainActivity.kt
@@ -3,14 +3,18 @@ package com.compose.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import cafe.adriel.voyager.navigator.Navigator
+import com.compose.app.AndroidApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
-
+            Navigator(screen = AndroidApp())
         }
     }
 }
