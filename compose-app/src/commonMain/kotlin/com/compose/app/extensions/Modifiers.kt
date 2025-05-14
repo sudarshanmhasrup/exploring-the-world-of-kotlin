b22967f460a6aa209a885619cf87c644cfc63b8d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-14 12:53:25 +0530

    Create composeAppModifier() extension function

--- compose-app/src/commonMain/kotlin/com/compose/app/extensions/Modifiers.kt
+++ compose-app/src/commonMain/kotlin/com/compose/app/extensions/Modifiers.kt
@@ -2,11 +2,19 @@ package com.compose.app.extensions
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 
 internal fun Modifier.backgroundContainerModifier(color: Color): Modifier {
     return this
         .background(color = color)
         .fillMaxSize()
+}
+
+internal fun Modifier.composeAppModifier(topPadding: Dp): Modifier {
+    return this
+        .fillMaxSize()
+        .padding(top = topPadding)
 }
