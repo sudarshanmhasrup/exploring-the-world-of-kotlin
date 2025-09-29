fa72c9ea295ad7ce2f4617b73d766d71dc4848b5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-29 22:38:56 +0530

    Configure Java 23 toolchain and Kotlin JVM target compatibility for the kotlin-playground module

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.application)
@@ -12,6 +14,20 @@ application {
 
 dependencies {
     testImplementation(libs.kotlin.test)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("23")
+    }
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_23)
+    }
 }
 
 tasks.test {
