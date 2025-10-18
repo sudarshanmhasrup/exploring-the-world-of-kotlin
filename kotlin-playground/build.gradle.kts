bc1ba82c3609b9d7ebf3f17982d30fa24ac6fe0b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-18 09:59:00 +0530

    Define the kotlin-playground module's configuration information from the version catalog

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
+group = libs.versions.kotlinPlayground.group.get()
+version = libs.versions.kotlinPlayground.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/KotlinPlayground"))
