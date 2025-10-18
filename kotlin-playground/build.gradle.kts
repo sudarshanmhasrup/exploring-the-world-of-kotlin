e23c03d37ad2c325bf52ffeaaf9eecbc9ab863a9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-18 10:03:43 +0530

    Configure Java 21 toolchain and set the Kotlin JVM target to 21 for kotlin-playground module

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.application)
     alias(libs.plugins.kotlin.jvm)
@@ -5,6 +7,20 @@ plugins {
 
 group = libs.versions.kotlinPlayground.group.get()
 version = libs.versions.kotlinPlayground.version.get()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("21")
+    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/KotlinPlayground"))
