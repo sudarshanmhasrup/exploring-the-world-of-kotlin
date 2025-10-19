570ef8c7eaf2d60a6196db99155ae27402501467
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-19 15:23:37 +0530

    Setup JVM toolchain and target Java 21 bytecode for the practice-solutions module

--- practice-solutions/build.gradle.kts
+++ practice-solutions/build.gradle.kts
@@ -1,9 +1,25 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
 group = libs.versions.practiceSolutions.group.get()
 version = libs.versions.practiceSolutions.version.get()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("21")
+    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/PracticeSolutions"))
