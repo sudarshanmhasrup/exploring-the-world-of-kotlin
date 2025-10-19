7226e3de149fcecaa6835cc484d98f73c987d9e2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-19 15:20:00 +0530

    Define the practice-solutions module's configuration information from the version catalog

--- practice-solutions/build.gradle.kts
+++ practice-solutions/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
 
 }
 
+group = libs.versions.practiceSolutions.group.get()
+version = libs.versions.practiceSolutions.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/PracticeSolutions"))
