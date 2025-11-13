b42cead8f7ab4e835cd8c0a5f4f47c2ff16f0c8c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-13 19:24:35 +0530

    Define the trying-kotlin module's configuration information from the version catalog

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
+group = libs.versions.tryingKotlin.group.get()
+version = libs.versions.tryingKotlin.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/trying-kotlin"))
