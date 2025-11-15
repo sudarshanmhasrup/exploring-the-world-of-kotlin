69787a0165a88d6e28b9c4f2015f316366bd5900
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-15 10:04:16 +0530

    Set JVM toolchain for the jvm() target of the compose-playground module

--- compose-playground/build.gradle.kts
+++ compose-playground/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
@@ -8,7 +10,19 @@ group = libs.versions.composePlayground.group.get()
 version = libs.versions.composePlayground.version.get()
 
 kotlin {
-    jvm("desktop")
+    jvm("desktop") {
+        java {
+            toolchain {
+                languageVersion = JavaLanguageVersion.of("21")
+            }
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
 
     sourceSets {
         val desktopMain by getting
