35e01b11c194ab25c4fa23da82aaeb7dc736ff31
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-15 14:53:41 +0530

    Add PackageDefinition.kt to the trying-kotlin module

---
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/overview/PackageDefinition.kt
@@ -0,0 +1,7 @@
+package com.trying.kotlin.overview  // Declaring the package "com.trying.kotlin.overview"
+
+import kotlin.math.PI // Importing the PI constant from the kotlin.math package
+
+fun main() {
+    println(message = "The value of PI is: $PI")
+}
