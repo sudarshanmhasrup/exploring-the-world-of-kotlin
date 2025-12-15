ce1ee704dcb7734a1d1bada7330f7b598fa5e4cd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-15 22:28:23 +0530

    Modify the PackageDefinition.kt to just include package declaration code and comment

--- trying-kotlin/src/main/kotlin/com/trying/kotlin/overview/PackageDefinition.kt
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/overview/PackageDefinition.kt
@@ -1,7 +1,5 @@
 package com.trying.kotlin.overview  // Declaring the package "com.trying.kotlin.overview"
 
-import kotlin.math.PI // Importing the PI constant from the kotlin.math package
-
 fun main() {
-    println(message = "The value of PI is: $PI")
+    println(message = """Hey! The package name of this file is: "com.trying.kotlin.overview".""")
 }
