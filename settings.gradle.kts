fb38c870944e8f838046a190beb61a8accae2b04
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-16 20:35:14 +0530

    Set repositoriesMode to FAIL_ON_PROJECT_REPOS in dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,7 @@ plugins {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google {
             mavenContent {
